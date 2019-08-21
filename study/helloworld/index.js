5bd1fb1319f35f42d313a2fa68c9bd0b22afea39
chandlersong <chandler605@gmail.com>
2019-08-21 21:29:54 +0800

    helloWorld: add example of how to import self modules

--- study/helloworld/index.js
+++ study/helloworld/index.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const hello = require('./libs/helloworld');
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.end('Hello, world.');
+    res.end(hello.sayHello("chandler"));
 });
 
 server.listen(port, () => {
-    console.log('Server listening on: http://localhost:%s', port)
-})
+    console.log('Server listening on: http://localhost:%s', port);
+});
---
+++ study/helloworld/libs/helloworld.js
@@ -0,0 +1,5 @@
+function sayHello(name){
+    return 'hello ' + name;
+}
+
+exports.sayHello = sayHello;
